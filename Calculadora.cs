7b13705a73cfbfece307b9b28beb18e50711b71d
Lara Ester <99409287+desprogramada@users.noreply.github.com>
2022-08-02 12:21:34 -0300

    Add files via upload

---
+++ Calculadora.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProjetosC_
+{
+    public class Program
+    {
+        static void Main(string[] args){
+            int num1, num2, resultado = 0;
+            string operacao;
+
+            Console.WriteLine("Digite o primeiro número: ");
+            num1 = int.Parse(Console.ReadLine());
+
+             Console.WriteLine("Digite a operação: ");
+            operacao = Console.ReadLine();
+
+             Console.WriteLine("Digite o segundo número: ");
+            num2 = int.Parse(Console.ReadLine());
+
+            switch(operacao){
+                case "+":
+                resultado = num1 + num2;
+                break;
+
+                case "-":
+                resultado = num1 - num2;
+                break;
+
+                case "*":
+                resultado = num1 * num2;
+                break;
+
+                case "/":
+                resultado = num1 / num2;
+                break;
+
+                default:
+                Console.WriteLine("Operação inválida");
+                break;
+
+            }
+
+            Console.WriteLine("{0} {1} {2} = {3}", num1,operacao,num2,resultado);
+
+            Console.ReadKey(true);
+        }
+    } 
+}
+
+ 
